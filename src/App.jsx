1df6a3d0e4aa6f027d73b3863dcbeca47ad685b5
Pallavi Bandarkar <bandarkarpallavi7@gmail.com>
2024-09-02 00:26:29 +0530

    Upadated some files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import React from "react"
+import React, { useState } from "react"
+import "./App.css"
 import { Route, Routes } from "react-router-dom"
 import Navbar from "./components/Navbar/Navbar.jsx"
 import Home from "./pages/Home/Home.jsx"
+import Login from "./components/Login/Login.jsx"
 function App() {
+  let [currState,setCurrState] = useState("false")
   return (
     <>
-      <Navbar/>
+      {currState?<Login setCurrState={setCurrState}/>:<></>}
+      <Navbar setCurrState={setCurrState}/>
       <Routes>
         <Route path="/" element={<Home/>}/>
       </Routes>
