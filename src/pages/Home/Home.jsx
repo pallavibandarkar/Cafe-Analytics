809532139c3fc5b48324d7a37a2fe1cf32f57510
Sneha-Gawas <19gawassneha@gmail.com>
2024-09-02 00:36:39 +0530

    Commited

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react"
 import Header from "../../components/Header/Header.jsx"
+
 export default function Home(){
     return(
         <>
         <Header/>
+        
         </>
     )
 }
